5972c5a327113e583c87016f417c49e3f6397522
Camilo Martinez <johnmarriv@alu.edu.gva.es>
2022-02-22 22:23:17 +0100

    Corrección realizada-faltaba for para los caminos

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -19,10 +19,15 @@ public class Bicicleta {
         int sum2 = 0;
         int sum3 = 0;
         
-        for(int i=0; i<camino1.length; i++)
-        {
+        for(int i=0; i<camino1.length; i++){
             sum1 += camino1[i];
+        }
+
+        for(int i=0; i<camino2.length; i++){
             sum2 += camino2[i];
+        }
+        
+        for(int i=0; i<camino3.length; i++){
             sum3 += camino3[i];
         }
         
